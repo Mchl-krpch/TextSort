afb3bd6edda838e5d6420b7df2da5828072e6d25
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-25 16:18:47 +0300

    Update text_output_functions.c

--- text_output_functions.c
+++ text_output_functions.c
@@ -1,8 +1,18 @@
-#ifndef ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
-#define ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
+#include <cstdio>
 
-void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file);
+#include "text_output_functions.h"
 
-void array_copy(char ***from, char ***to, size_t array_len);
+void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file) {
+    for (int str = 0; str < number_of_strings; str++) {
+        fprintf(output_file, arr_pointers[str]);
+        fprintf(output_file, "\n");
+    }
+}
 
-#endif //ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
+void array_copy(char ***from, char ***to, size_t array_len) {
+    for (int str = 0; str <array_len; str++) {
+        to[str] = from[str];
+    }
+    //*(*from + 0) = '0';
+    printf("origin %s\n", *to[0]);
+}
