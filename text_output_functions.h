2652dc86ff94a65b9c5a8e22f6397d80261e0369
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-25 16:19:03 +0300

    Create text_output_functions.h

---
+++ text_output_functions.h
@@ -0,0 +1,8 @@
+#ifndef ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
+#define ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
+
+void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file);
+
+void array_copy(char ***from, char ***to, size_t array_len);
+
+#endif //ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
