526ca761cc05d9c94fb5d0fc5e3344200b80792e
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-26 14:00:26 +0300

    Update text_output_functions.h

--- text_output_functions.h
+++ text_output_functions.h
@@ -1,8 +1,10 @@
 #ifndef ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
 #define ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
 
+///\brief печатает массив символов в текстовый файл
+/// \param arr_pointers ссылка на массив типа (char **), массив состоит из указателей на начала строк
+/// \param number_of_strings количество строк для печати
+/// \param output_file текстовый файл, куда происходит печать
 void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file);
-
-void array_copy(char ***from, char ***to, size_t array_len);
 
 #endif //ONEGIN6_TEXT_OUTPUT_FUNCTIONS_H
