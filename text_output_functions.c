2e7f207a545b298a304aacf2577c5c0bf43922c9
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-26 14:02:36 +0300

    Update text_output_functions.c

--- text_output_functions.c
+++ text_output_functions.c
@@ -8,11 +8,3 @@ void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file)
         fprintf(output_file, "\n");
     }
 }
-
-void array_copy(char ***from, char ***to, size_t array_len) {
-    for (int str = 0; str <array_len; str++) {
-        to[str] = from[str];
-    }
-    //*(*from + 0) = '0';
-    printf("origin %s\n", *to[0]);
-}
