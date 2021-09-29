9fc7d276ef0389e3192e6f76a90ae9a6433eaeca
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-29 03:29:56 +0300

    Update text_output_functions.c

--- text_output_functions.c
+++ text_output_functions.c
@@ -1,10 +1,17 @@
-#include <cstdio>
-
 #include "text_output_functions.h"
 
-void text_to_file(char **arr_pointers, int number_of_strings, FILE *output_file) {
-    for (int str = 0; str < number_of_strings; str++) {
-        fprintf(output_file, arr_pointers[str]);
-        fprintf(output_file, "\n");
+void print_text_to_file(Line_info *pointers, int number_of_strings, FILE *output_file) {
+    for (int str = 0; str < number_of_strings - 1; str++) {
+        my_fprintf(output_file, pointers[str].ptr_begin);
     }
 }
+
+void my_fprintf(FILE *output_file, char *pointers) {
+    int i = 0;
+    while (pointers[i] != '\n') {
+        fputc(pointers[i], output_file);
+        i++;
+    }
+    fputc('\n', output_file);
+}
+
