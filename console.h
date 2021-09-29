4825c7ec3e9f5fd66e472a29e51f0330589087f3
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-29 19:51:16 +0300

    Update console.h

--- console.h
+++ console.h
@@ -4,11 +4,8 @@
 ///\param RUS_wCODE stores the encoding number windows1251
 unsigned int const RUS_wCODE = 1251;
 
-///\param help stores the message that the program prints if it has only one (initial) console parameter
-char *const help = "\tThis is a text sorting program\nSpecify the first argument argv [1] the name of the sorted file located"
-                   "in the program folder\nthen enter sorting mode - begin string - sorts the text by arranging lines\nbased"
-                   "on lexicographic comparison, the comparison starts from the beginning of the line\nend string mode - "
-                   "sorts similarly, but starts comparing characters from the end\n";
+///\brief special char const. Use it when you need to explain user how to work your program
+extern char *const help;
 
 ///\brief puts Russian encoding
 void encoding_set_rus();
