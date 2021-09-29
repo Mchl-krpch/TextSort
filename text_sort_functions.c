abf68d14fa29dac64c74f1a648d1689c16b464e0
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-29 03:30:36 +0300

    Update text_sort_functions.c

--- text_sort_functions.c
+++ text_sort_functions.c
@@ -1,141 +1,104 @@
 #include <cstdio>
-#include <cstdlib>
 #include <cassert>
 #include <cstdint>
 
 #include "text_sort_functions.h"
+#include "text_input_functions.h"
 
-int choose_method_of_sorting() {
-    int temporary_method = INT_NAN;
-    printf("How should i sort strings?\n"
-           "0.begin str\n"
-           "1.end str\n");
-
-    if (scanf_s("%d", &temporary_method) != 1) {
-        printf("Incorrect data: use ints");
-        exit(0);
-    }
-    return temporary_method;
-}
-
-void my_qsort(void *arr, size_t n_of_element, size_t size_of_element, int (*cmp)(const void *a, const void *b)) {
+void my_qsort(void *arr, size_t n_of_element, size_t size_of_element, size_t mode_of_sorting,
+              int (*string_cmp)(const void *a, const void *b, int mode)) {
     assert (arr != nullptr);
-    assert (cmp != nullptr);
+    assert (string_cmp != nullptr);
+    assert(string_cmp != nullptr);
 
-    char *start = (char *) arr;
+    Line_info *start = (Line_info *) arr;
 
     int right = (int) n_of_element - 1;
     int last = 0;
     int tmp = right / 2;
 
-    if (0 >= right) return;
+    if (0 >= right) {
+        return;
+    }
 
-    swap(start, start + tmp * size_of_element, size_of_element);
+    partition(start, &right, &last, &tmp, size_of_element, mode_of_sorting, string_cmp);
 
-    for (int i = 1; i <= right; ++i) {
-        if (cmp(start + i * size_of_element, start) < 0) {
-            ++last;
-            swap(start + i * size_of_element, start + last * size_of_element, size_of_element);
+    my_qsort((Line_info *) start, last, size_of_element, mode_of_sorting, string_cmp);
+    my_qsort(((Line_info *) start + last + 1), n_of_element - last - 1,
+             size_of_element, mode_of_sorting, string_cmp);
+}
+
+void partition(void *start, int *right, int *last, int *tmp, size_t size_of_element, size_t mode_of_sorting,
+               int (*string_cmp)(const void *a, const void *b, int mode)) {
+    assert(string_cmp != nullptr && "partition");
+    assert(start != nullptr && "partition");
+    assert(size_of_element != NULL && "partition");
+    assert(string_cmp != nullptr && "partition");
+
+
+    swap(((Line_info *) start), ((Line_info *) start + *tmp), size_of_element);
+
+    for (int i = 1; i <= *right; ++i) {
+        if (string_cmp(((Line_info *) start + i), ((Line_info *) start), mode_of_sorting) < 0) {
+            ++(*last);
+            swap(((Line_info *) start + i), ((Line_info *) start + *last), sizeof(Line_info));
+        }
+        if (*last >= 14) {
+            return;
         }
     }
 
-    swap(start + last * size_of_element, start, size_of_element);
-
-    my_qsort(start, last, size_of_element, cmp);
-    my_qsort(start + (last + 1) * size_of_element, n_of_element - last - 1, size_of_element, cmp);
+    swap(((Line_info *) start + *last), (Line_info *) start, size_of_element);
 }
 
-bool is_alpha(char const *ch) {
-    assert (ch != nullptr && "is_alpha: &ch = nullptr");
+bool is_letter(char ch) {
+    return ('А' <= ch && ch <= 'Я') || ('а' <= ch && ch <= 'я');
+}
 
-    wchar_t bad_chars[] = L"—«» !;,.:()\"";
-
-    for (int i = 0; i < sizeof(bad_chars) / sizeof(*bad_chars); i++) {
-        if (*ch == bad_chars[i]) return false;
+int str_cmp_with_mode(const void *a, const void *b, int mode) {
+    if (mode == SORTING_BEGIN) {
+        return string_cmp(((Line_info *) a)->ptr_begin, ((Line_info *) b)->ptr_begin, 1);
     }
-
-    return true;
+    if (mode == SORTING_END) {
+        return string_cmp(((Line_info *) a)->ptr_begin + ((Line_info *) a)->str_len - 1,
+                          ((Line_info *) b)->ptr_begin + ((Line_info *) b)->str_len - 1, -1);
+    }
+    return 1;
 }
 
-int cmp(const void *a, const void *b) {
+// int str_cmp_forward(const void *a, const void *b)
+// return string_cmp(char*(a), ...b, +1);
+// int str_cmp_rev(const void *a, const void *b)
+//
+// int string_cmp(const void *a, const void *b, int step)
+int string_cmp(const void *a, const void *b, int step) {
     assert (a != nullptr && "cmp: &a = nullptr");
     assert (b != nullptr && "cmp: &b = nullptr");
 
-    char *ch1 = *(char **) a;
-    char *ch2 = *(char **) b;
 
-    while (is_alpha(ch1) == 0) ch1++;
+    char *ch1 = (char *) a;
+    char *ch2 = (char *) b;
 
-    while (is_alpha(ch2) == 0) ch2++;
+    while (!is_letter(*ch1)) {
+        ch1 += step;
+    }
 
-    while (equal(ch1, ch2) == 1) {
-        ch1++;
-        ch2++;
+    while (!is_letter(*ch2)) {
+        ch2 += step;
+    }
+
+    if (*ch1 == *ch2) {
+        ch1 += step;
+        ch2 += step;
     }
 
     return *ch1 - *ch2;
 }
 
-int cmp_reverse(const void *a, const void *b) {
-    assert (a != nullptr && "cmp: &a = nullptr");
-    assert (b != nullptr && "cmp: &b = nullptr");
-
-    char *ch1 = *(char **) a;
-    char *ch2 = *(char **) b;
-
-    while (*ch1 != '\0') ch1++;
-    while (*ch2 != '\0') ch2++;
-    ch1--;
-    ch2--;
-
-
-    while (is_alpha(ch1) == 0) ch1--;
-
-    while (is_alpha(ch2) == 0) ch2--;
-
-    while (equal(ch1, ch2) == 1) {
-        ch1--;
-        ch2--;
-    }
-
-    return *ch1 - *ch2;
-}
-
-int equal(const void *b, const void *a) {
-    char ch1 = *(char *) a;
-    char ch2 = *(char *) b;
-
-    if (ch1 == ch2) return 1;
-    return 0;
-}
-
-int swap(void *elem1, void *elem2, size_t size_of_element) {
-    assert (elem1 != nullptr && "swap: &elem1 = nullptr");
-    assert (elem2 != nullptr && "swap: &elem2 = nullptr");
-
-    while (size_of_element > 0) {
-        if (size_of_element >= 8) {
-            uint64_t tmp = *(uint64_t *) elem1;
-            *(uint64_t *) elem1 = *(uint64_t *) elem2;
-            *(uint64_t *) elem2 = tmp;
-        } else {
-            uint8_t tmp = *(uint8_t *) elem1;
-            *(uint8_t *) elem1 = *(uint8_t *) elem2;
-            *(uint8_t *) elem2 = tmp;
-        }
-
-        size_t step = (size_of_element >= 8 ? 8 : 1);
-        size_of_element -= step;
-
-        elem1 = (char *) elem1 + step;
-        elem2 = (char *) elem2 + step;
-    }
+int swap(Line_info *elem1, Line_info *elem2, size_t size_of_element) {
+    Line_info temp = *(Line_info *) elem1;
+    *elem1 = *elem2;
+    *elem2 = temp;
 
     return 0;
-}
-
-void array_copy(char **from, char **to, size_t array_len) {
-    for (int str = 0; str < array_len; str++) {
-        to[str] = from[str];
-    }
 }
