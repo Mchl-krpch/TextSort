139462c7ea032b7ff21eb1ecaed37a500af6f297
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-29 03:27:42 +0300

    Update console.h

--- console.h
+++ console.h
@@ -1,20 +1,20 @@
 #ifndef ONEGIN6_CONSOLE_H
 #define ONEGIN6_CONSOLE_H
 
-///\param RUS_wCODE хранит в себе номер кодировки windows1251
+///\param RUS_wCODE stores the encoding number windows1251
 unsigned int const RUS_wCODE = 1251;
 
-///\param help хранит в себе сообщение, которое печатает программа, если имеет всего один (начальный) параметр консоли
+///\param help stores the message that the program prints if it has only one (initial) console parameter
 char *const help = "\tThis is a text sorting program\nSpecify the first argument argv [1] the name of the sorted file located"
                    "in the program folder\nthen enter sorting mode - begin string - sorts the text by arranging lines\nbased"
                    "on lexicographic comparison, the comparison starts from the beginning of the line\nend string mode - "
                    "sorts similarly, but starts comparing characters from the end\n";
 
-///\brief ставит русскую кодировку
+///\brief puts Russian encoding
 void encoding_set_rus();
 
-///\brief выводит сообщение help для пользователея, который ничего не ввел в консоль
-///(предположительно запускает программу в первый раз)
+///\brief prints a "help" message for a user who hasn't entered anything into the console
+///(presumably runs the program for the first time)
 void print_help_onegin();
 
 #endif //ONEGIN6_CONSOLE_H
