4d247d8948e56ae0a4504828395e0ea47edaf157
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-30 23:10:35 +0300

    Update console.c

--- console.c
+++ console.c
@@ -2,17 +2,19 @@
 #include <Windows.h>
 
 #include "console.h"
+unsigned int const RUS_wCODE = 1251;
 
 ///\param help stores the message that the program prints if it has only one (initial) console parameter
-char *const help = "\tThis is a text sorting program\nSpecify the first argument argv [1] the name of the sorted file located"
-                   "in the program folder\nthen enter sorting mode - begin string - sorts the text by arranging lines\nbased"
-                   "on lexicographic comparison, the comparison starts from the beginning of the line\nend string mode - "
-                   "sorts similarly, but starts comparing characters from the end\n";
+const char *const help = "\tThis is a text sorting program"
+                         "\nSpecify the first argument argv [1] the name of the sorted file located in the program\n"
+                         "folder then enter sorting mode - begin string - sorts the text by arranging lines\n"
+                         "based on lexicographic comparison, the comparison starts from the beginning of the line\n"
+                         "end string mode - sorts similarly, but starts comparing characters from the end\n";
 
 void encoding_set_rus() {
-    setlocale(LC_ALL, "rus");
-    SetConsoleCP(RUS_wCODE);
-    SetConsoleOutputCP(RUS_wCODE);
+    setlocale            (LC_ALL, "rus");
+    SetConsoleCP         (RUS_wCODE);
+    SetConsoleOutputCP   (RUS_wCODE);
 
     printf("\tRussian is set\n");
 }
