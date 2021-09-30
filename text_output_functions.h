a13f3d37a8654ce65a07600515f20fb9ad150f9d
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2021-09-30 23:11:41 +0300

    Update text_output_functions.h

--- text_output_functions.h
+++ text_output_functions.h
@@ -7,7 +7,7 @@
 /// \param arr_pointers ссылка на массив типа (char **), массив состоит из указателей на начала строк
 /// \param number_of_strings количество строк для печати
 /// \param output_file текстовый файл, куда происходит печать
-void print_text_to_file(Line_info *sorted_pointers, int number_of_strings, FILE *output_file);
+void print_text_to_file(Line_info *sorted_pointers, size_t number_of_strings, FILE *output_file);
 
 ///\brief save output sorted text in special text file
 /// \param output_file file which store your sorted text
